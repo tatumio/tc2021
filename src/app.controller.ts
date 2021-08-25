aab1e77e7f9473def6b114e1b50a608e5bdb790e
Samuel Sramko <samuel.sramko@tatum.io>
2021-08-25 13:24:33 +0200

    add recaptcha

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,28 @@
-import {
-  Body,
-  Controller,
-  Post,
-  UploadedFile,
-  UseInterceptors,
-} from '@nestjs/common';
-import { AppService } from './app.service';
-import { FileInterceptor } from '@nestjs/platform-express';
-import { Multer } from 'multer';
+import {Body, Controller, Post, UploadedFile, UseInterceptors,} from '@nestjs/common';
+import {AppService} from './app.service';
+import {FileInterceptor} from '@nestjs/platform-express';
+import {Multer} from 'multer';
+import {IsIn, IsNotEmpty, MaxLength} from 'class-validator';
+import {Currency} from '@tatumio/tatum';
+
+class Nft {
+  @IsNotEmpty()
+  @MaxLength(100)
+  public name: string;
+
+  @IsNotEmpty()
+  @MaxLength(100)
+  public description: string;
+
+  @IsNotEmpty()
+  @MaxLength(100)
+  @IsIn([Currency.MATIC, Currency.BSC, Currency.FLOW])
+  public chain: Currency;
+
+  @IsNotEmpty()
+  @MaxLength(3000)
+  public token: string;
+}
 
 @Controller()
 export class AppController {
@@ -17,8 +32,8 @@ export class AppController {
   @UseInterceptors(FileInterceptor('file'))
   public async createNFT(
     @UploadedFile() file: Multer.File,
-    @Body() { name, description, chain }: any,
+    @Body() { name, description, chain, token }: Nft,
   ) {
-    return this.appService.createNFT(file.buffer, name, description, chain);
+    return this.appService.createNFT(file.buffer, name, description, chain, token);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
+import {ValidationPipe} from '@nestjs/common';
 
 process.env.TATUM_API_KEY = '4966d428-9507-45cb-9f90-02cca00674bd_100';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(helmet());
+  app.useGlobalPipes(new ValidationPipe());
+  app.use(helmet({
+    contentSecurityPolicy: false
+  }));
   await app.listen(3000);
 }
 bootstrap();
